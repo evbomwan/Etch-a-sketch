ad06ee895cf05c37b8e623187df4f8200fb987cb
evbomwan <evbomwanc@gmail.com>
2026-05-16 21:23:57 +0100

    added a buttin asking for user input

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const button = document.createElement("button");
+button.textContent = "Click to input number";
+document.body.prepend(button);
+button.addEventListener("click", ()=>{
+    alert(prompt("Enter the number of squares per grid you want"))
+    
+})
 const gridContainer = document.getElementById("container");
 gridContainer.setAttribute("style",
                             `display: flex; 
@@ -8,7 +15,7 @@ gridContainer.setAttribute("style",
 )
 for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
-    div.setAttribute("style","border: 1px solid black; flex-basis:90px; height: 40px");
+    div.setAttribute("style","flex-basis:90px; height: 40px");
     gridContainer.appendChild(div);
     let red = Math.floor(Math.random() * 255);
     let green = Math.floor(Math.random() * 255);
@@ -17,7 +24,7 @@ for (let i = 0; i < 256; i++) {
     div.addEventListener("mouseenter", ()=>{
         div.style.backgroundColor = color
     })
-    div.addEventListener("mouseleave", ()=>{
-        div.style.backgroundColor = ""
-    })
+    // div.addEventListener("mouseleave", ()=>{
+    //     div.style.backgroundColor = ""
+    // })
 }
