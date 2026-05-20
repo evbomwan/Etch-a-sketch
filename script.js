abf61693c0a7ce7c3bfb0a42dc214e4db5636146
evbomwan <evbomwanc@gmail.com>
2026-05-20 16:05:25 +0100

    set maximum user input value to 100

--- script.js
+++ script.js
@@ -3,20 +3,12 @@ button.textContent = "Click to input number";
 document.body.prepend(button);
 button.addEventListener("click", getUserInput)
 function getUserInput (){
-    gridContainer.remove();
-    const newGridContainer = document.createElement("div");
-    newGridContainer.setAttribute("style",
-                            `display: flex; 
-                            align-items:center;
-                            flex-wrap: wrap;
-                            width: 1500px;
-                            height: 640px;
-                            `
-    )
-    document.body.append(newGridContainer);
-    newGridContainer.innerHTML = "";
-    const input = prompt("Enter number grid rows and columns")
-    for (let i = 0; i < (input * input); i++) {
+    gridContainer.innerHTML = "";
+    const input = prompt("Enter number grid rows and columns, not more than a 100")
+    if (input > 100){
+        return prompt("Enter a valid number")
+    } else {
+         for (let i = 0; i < (input * input); i++) {
         const div = document.createElement("div");
         const size = 1500 / input;
         div.setAttribute("style",
@@ -24,7 +16,7 @@ function getUserInput (){
                         height: ${size}px;
                         b0x-sizing:border-box;
                          `);
-        newGridContainer.appendChild(div);
+        gridContainer.appendChild(div);
         let red = Math.floor(Math.random() * 255);
         let green = Math.floor(Math.random() * 255);
         let blue = Math.floor(Math.random() * 255);
@@ -32,7 +24,12 @@ function getUserInput (){
         div.addEventListener("mouseenter", ()=>{
         div.style.backgroundColor = color
     })
+    div.addEventListener("mouseleave", ()=>{
+        div.style.backgroundColor = ""
+    })
     }
+    }
+   
 }
     
 const gridContainer = document.getElementById("container");
